3f1c8d3de7d1bcc02ae66a401682cee9f04df3c4
kmcanney <kmcanney@gmail.com>
2023-07-26 16:00:59 -0500

    Adding source

---
+++ bandname.py
@@ -0,0 +1,10 @@
+print("Handy-Dandy Band-Name Generator")
+
+print("For every cool band name out there, there are countless terrible 
+names.\nNaming is hard, so let's take out some of the guesswork.\n")
+
+street = input("What was the name of the street where you grew up? ")
+pet = input("\nWhat was the name of your first pet? ")
+number = int(input("\nWhat's your lucky number? "))
+
+print(f"\nYour new band is {street} {pet} {number}.\nGo forth and rock.")
